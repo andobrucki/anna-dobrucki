6e3401961d80ff440d69c579b9364b45918983e0
andobrucki <annadobrucki@gmail.com>
2026-01-12 09:21:39 +0100

    js encoding for email

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ const cursor = document.getElementById("cursor-circle");
 document.body.addEventListener("mousemove", function (e) {
   (cursor.style.left = e.clientX + "px"), (cursor.style.top = e.clientY + "px");
 });
+
+function sendMail() {
+  const user = "hello";
+  const domain = "annadobrucki.com";
+  window.location.href = "mailto:" + user + "@" + domain;
+}
